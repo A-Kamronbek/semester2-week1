ec5542eee4b56cb2dbf687d2cb9df4465a9a73ba
Kamronbek <118995322+A-Kamronbek@users.noreply.github.com>
2026-01-30 12:31:22 +0000

    new

--- session_2/1_aggregators/tasks.sql
+++ session_2/1_aggregators/tasks.sql
@@ -7,3 +7,5 @@
 -- 2. Load this script: .read tasks.sql
 -- 3. Exit SQLite: .exit
 
+-- SELECT Continent, AVG(Population) FROM Countries GROUP BY Continent
+SELECT Continent, SUM(Population) FROM Countries GROUP BY Continent
--- session_2/2_joins/tasks.sql
+++ session_2/2_joins/tasks.sql
@@ -7,3 +7,7 @@
 -- 2. Load this script: .read tasks.sql
 -- 3. Exit SQLite: .exit
 
+-- SELECT id AS 'post' FROM posts where deleted;
+-- SELECT username FROM users WHERE username LIKE 'a%';
+-- SELECT posts.id as post, username FROM posts JOIN users on posts.user_id = users.id 
+-- WHERE content LIKE '%small%';
